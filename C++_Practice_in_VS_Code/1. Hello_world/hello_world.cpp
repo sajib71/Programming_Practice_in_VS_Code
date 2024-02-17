377644410335b0d2cf9a8cee1bf37c7bdee8c4ba
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-17 22:07:40 +0600

    Write Your First Code "Hello world"

---
+++ C++_Practice_in_VS_Code/1. Hello_world/hello_world.cpp
@@ -0,0 +1,6 @@
+#include<iostream>                        // insert C++ header file
+using namespace std;                     // we can use names for objects and variables from the standard library
+int main (){
+    cout<< "Hello World";
+    return 0;
+}
