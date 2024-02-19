a266bd44859d29b828c24f121033aaa6d576727c
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-19 22:32:47 +0600

    Subtract Assignment Operator

---
+++ C++_Practice_in_VS_Code/3. Assignment_Operator/3. Subtraction_Assignment/Subtact_assignment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int value = 3;
+    cout << "Origianl Value is: "<< value <<endl;
+    value -=2;
+    cout <<"Value after subtraction is: " << value;
+    return 0;
+}
