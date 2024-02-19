0dce508fcb1c3db249e158d8022d90088b5064c9
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-19 22:32:28 +0600

    Addition Assignment Operator

---
+++ C++_Practice_in_VS_Code/3. Assignment_Operator/2. Addition_Assignment/addition.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int value = 3;
+    cout << "Origianl Value is: "<< value <<endl;
+    value +=2;
+    cout <<"Value after addition is: " << value;
+    return 0;
+}
