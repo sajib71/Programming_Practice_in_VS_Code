f282aabbf530d2d7a9b29b276ce1f8c8ce4b3991
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-20 15:14:21 +0600

    Division Assignment

---
+++ C++_Practice_in_VS_Code/3. Assignment_Operator/5. Division_Assignment/Division_assignment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int value = 10;
+    cout << "Value is: "<< value <<endl;
+    value /=2;
+    cout <<"Value after Division is: " << value;
+    return 0;
+}
