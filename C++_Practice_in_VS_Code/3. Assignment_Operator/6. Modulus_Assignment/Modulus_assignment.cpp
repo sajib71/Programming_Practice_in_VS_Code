cef55e52806afd8b8c72453fe90788aed79d363a
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-20 15:14:39 +0600

    Modulus Assignment

---
+++ C++_Practice_in_VS_Code/3. Assignment_Operator/6. Modulus_Assignment/Modulus_assignment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int value = 9;
+    cout << "Value is: "<< value <<endl;
+    value %=2;
+    cout <<"Modulus is: " << value;
+    return 0;
+}
