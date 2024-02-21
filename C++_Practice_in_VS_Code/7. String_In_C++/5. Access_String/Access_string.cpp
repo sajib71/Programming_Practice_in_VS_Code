5887b7afbf2b824388f679813c69a9217584c7ca
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-21 20:10:34 +0600

    Access String

---
+++ C++_Practice_in_VS_Code/7. String_In_C++/5. Access_String/Access_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    string name = "Hasan";
+    cout << name[0];        // "0" means it indicates which specific string will print
+    return 0;
+}
+
+// OUTPUT : H
