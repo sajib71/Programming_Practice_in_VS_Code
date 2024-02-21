4f2c74709a825bbc848e2ec450a642c1c260f776
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-21 20:08:24 +0600

    String Length

---
+++ C++_Practice_in_VS_Code/7. String_In_C++/4. String_Length/Count_String_Length.cpp
@@ -0,0 +1,12 @@
+/*
+Count how many charecter in a string
+*/
+#include<iostream>
+using namespace std;
+int main (){
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length();
+    return 0;
+}
+
+// OUTPUT : The length of the txt string is: 26
