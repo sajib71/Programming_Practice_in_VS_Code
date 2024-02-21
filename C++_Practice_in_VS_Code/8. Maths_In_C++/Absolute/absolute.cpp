46c5d2ef3a7846e98be6812e7c24fa5c16a46543
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-21 20:10:59 +0600

    Absolute

---
+++ C++_Practice_in_VS_Code/8. Maths_In_C++/Absolute/absolute.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main (){
+    int x = -5;
+    int result;
+    result = abs(x);
+    cout << "Absolute: "<< result;
+    return 0;
+}
