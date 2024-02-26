35ed86a3682debf50a12f6c39ac972311c653465
Sanaullah Sajib <sajib71208@gmail.com>
2024-02-26 16:15:42 +0600

    Character, Number, Vowel, Consonant, Odd, Even, Positive, Negative

---
+++ C++_Practice_in_VS_Code/Problem/3. Big_Problem_01/problm.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main (){
+    char input;
+    cout << "Enter An Input: ";
+    cin >> input;
+    cout << "You Have Enter: " << input <<endl;
+    if((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z')){
+        cout << "It's a Charecter" <<endl;
+        if ((input == 'a' || input == 'A' || input == 'e' || input == 'E' ||
+             input == 'i' || input == 'I' || input == 'o' || input == 'O' || input == 'u' || input == 'U')){
+            cout << "It's a Vowel" <<endl;
+        }
+        else {
+            cout << "It's a Consonant" << endl;
+        }
+        if((input >= 'A' && input <= 'Z')){
+            cout << "It's a Capital Letter";
+        }
+        else {
+            cout << "It's a Small Letter";
+        }
+    }
+    else {
+        cout << "it's not a charecter" <<endl;
+    }
+    if (input > 0 && input % 2 == 0){
+        cout << "It' Even";
+    }
+    else {
+        cout << "It's Odd";
+    }
+    if (input - 1)
+    return 0;
+}
